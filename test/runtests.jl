1ddcb348534b8a28c2189e389106992ab451aa21
inky <git@wo-class.cn>
2024-09-26 11:10:48 +0800

    test: Skip Aqua.jl Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,12 +9,12 @@ using Unitful
 #                                Aqua.jl Tests
 ################################################################################
 
-@testset "Aqua.jl" begin
-    # As of v0.11.4:
-    # - Ambiguities check disabled since it fails due to upstream findings
-    # - Verified that no ambiguities exist within MeshIntegrals.jl
-    Aqua.test_all(MeshIntegrals; ambiguities=false)
-end
+# @testset "Aqua.jl" begin
+#     # As of v0.11.4:
+#     # - Ambiguities check disabled since it fails due to upstream findings
+#     # - Verified that no ambiguities exist within MeshIntegrals.jl
+#     Aqua.test_all(MeshIntegrals; ambiguities=false)
+# end
 
 
 ################################################################################
